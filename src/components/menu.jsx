b1b6cd89b08730cdd2893f2893ff6eb9bbe65001
cyril <cyril@haberer.fr>
2025-10-23 16:51:34 +0200

    manage the menu button state acording to the page

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -2,18 +2,28 @@ import React from "react"
 import Nom from "../images/NOM_PAGE_D_ACCUEIL.svg"
 import MiniLogo from "../images/LOGO_PETIT_HEADER.svg"
 
-export default function Menu({ isHome = true }) {
-    return (
-        <div className='Menu'>
-            {isHome ? (
-                <img className='NomInfam' src={Nom} alt="Nom page d'acceuil" />
-            ) : (
-                <img className='LogoInfam' src={MiniLogo} alt="Mini logo header" />
-            )}
-            <div className="menu">
-                <button>Projets</button>
-                <button>À propos</button>
-            </div>
-        </div>
-    )
+export default function Menu({ isHome = true, isAbout = false, isProject = false }) {
+    const className = [
+    "Selector",
+    isHome && "Home",
+    isAbout && "About",
+    isProject && "Projets"
+  ]
+    .filter(Boolean) 
+    .join(" "); 
+
+  return (
+    <div className="Menu">
+      {isHome ? (
+        <img className="NomInfam" src={Nom} alt="Nom page d'accueil" />
+      ) : (
+        <img className="LogoInfam" src={MiniLogo} alt="Mini logo header" />
+      )}
+
+      <div className={className}>
+        <button className="ButtonProject">Projets</button>
+        <button className="ButtonAbout">À propos</button>
+      </div>
+    </div>
+  );
 }
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,8 +4,8 @@ import LogoAnimated from "../components/image_animated"
 export default function About() {
     return (
         <div className="Page">
-            <Menu isHome={false} />
-            <LogoAnimated isHome={false}/>
+            <Menu isHome={false} isAbout={true} isProject={false} />
+            <LogoAnimated isHome={false} isAbout={true} isProject={false}/>
         </div>
     )
 }
