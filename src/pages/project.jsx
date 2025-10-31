cdf5d30485ed3227bddbd1e12f7a3d232812d505
cyril <cyril@haberer.fr>
2025-10-31 22:40:37 +0100

    quick start whith the project page

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -4,7 +4,18 @@ export default function Projects() {
     return (
         <div className="Page ProjectsPage">
             <Menu isHome={false} isAbout={false} isProject={true} />
-            <h1>page project</h1>
+            <div className="ListProjects">
+                <h3>Encore</h3>
+                <h3>High-lo</h3>
+                <h3>Plates-Formes</h3>
+                <h3>Lvline</h3>
+                <h3>LégendesIndusties</h3>
+                <h3>Covers/Posters</h3>
+            </div>
+            <main>
+                <div className="detail">encore</div>
+                <div className="images">image ici</div>
+            </main>
         </div>
     )
 }
