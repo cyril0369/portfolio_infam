bb5bfeb806c2f8cfb3d9cef88a0489b4d8e7eeb2
cyril <cyril@haberer.fr>
2025-11-07 10:39:21 +0100

    responsive + fixe the animation

--- src/components/image_animated.jsx
+++ src/components/image_animated.jsx
@@ -12,30 +12,42 @@ export default function LogoAnimated({ isHome = true }) {
     const b = 50;
 
     useEffect(() => {
-        let imageX = window.innerWidth / 2
-        let imageY = window.innerHeight / 2
-
-        const handleMouseMove = e => {
         const image = logoRef.current
         if (!image) return
+
+        let imageX = window.innerWidth / 2
+        let imageY = window.innerHeight / 2
+        let targetX = imageX
+        let targetY = imageY
 
         const width = window.innerWidth
         const height = window.innerHeight
         const cx = width / 2
         const cy = height / 2
-        const px = e.clientX
-        const py = e.clientY
 
-        const hit = pointOrEllipseIntersection(px, py, cx, cy, a, b)
-        imageX = lerp(imageX, hit.x, 0.05)
-        imageY = lerp(imageY, hit.y, 0.05)
-
-        image.style.top = `${imageY - 300}px`
-        image.style.left = `${imageX}px`
+        // Quand la souris bouge → on change la cible
+        const handleMouseMove = e => {
+            const px = e.clientX
+            const py = e.clientY
+            const hit = pointOrEllipseIntersection(px, py, cx, cy, a, b)
+            targetX = hit.x
+            targetY = hit.y
         }
+        const animate = () => {
+            imageX = lerp(imageX, targetX, 0.05)
+            imageY = lerp(imageY, targetY, 0.05)
+
+            image.style.top = `${imageY - 300}px`
+            image.style.left = `${imageX}px`
+
+            requestAnimationFrame(animate)
+        }
+        
 
         document.addEventListener("mousemove", handleMouseMove)
+        requestAnimationFrame(animate)
         return () => document.removeEventListener("mousemove", handleMouseMove)
+
     }, [])
 
     return (
