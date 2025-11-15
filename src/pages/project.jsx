06efcafc014916d0325b18a9c509dd7be683644d
cyril <cyril@haberer.fr>
2025-11-15 11:26:05 +0100

    code cleaning

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
-import Menu from "../components/menu"
-import Marquee from "../components/marquee_project"
-import ProjectsDisplay from "../components/projects_display"
+import Menu from "../components/menu";
+import Marquee from "../components/marquee_project";
+import ProjectsDisplay from "../components/projects_display";
 import projects from "../object/projects_objects.json";
 
 export default function Projects() {
+  const [selectedKey, setSelectedKey] = useState("aboueb_beat_tape");
+  const selectedProject = projects[selectedKey];
 
-    const [selectedKey, setSelectedKey] = useState("aboueb_beat_tape");
-    const selectedProject = projects[selectedKey];
-
-    return (
-        <div className="Page ProjectsPage">
-            <Menu isHome={false} isAbout={false} isProject={true} />
-            <Marquee onSelectProject={setSelectedKey}/>
-            <ProjectsDisplay selectedProject={selectedProject} selectedKey={selectedKey} />
-
-        </div>
-    )
+  return (
+    <div className="Page ProjectsPage">
+      <Menu isHome={false} isAbout={false} isProject={true} />
+      <Marquee onSelectProject={setSelectedKey} />
+      <ProjectsDisplay
+        selectedProject={selectedProject}
+        selectedKey={selectedKey}
+      />
+    </div>
+  );
 }
