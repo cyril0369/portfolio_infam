c1eb394ebb89995701ec67b90b3d99698e0e9880
cyril <cyril@haberer.fr>
2025-11-15 11:25:13 +0100

    resets the scroll to zero when you arrive at a new project

--- src/components/projects_display.jsx
+++ src/components/projects_display.jsx
@@ -1,40 +1,45 @@
-
+import { useRef, useEffect } from "react";
 import StarsProject from "../data/graphic_element/ÉTOILES_PROJET.svg";
 
+export default function ProjectsDisplay({ selectedProject, selectedKey }) {
+  const carouselRef = useRef(null);
 
-export default function ProjectsDisplay({selectedProject, selectedKey}) {
+  useEffect(() => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollTop = 0;
+    }
+  }, [selectedKey]);
+  return (
+    <main>
+      <div className="detail">
+        <h1>{selectedProject.title}</h1>
+        <h4>{selectedProject.date}</h4>
+        <br />
+        <p>{selectedProject.description}</p>
+        <br />
+        <img src={StarsProject} alt="etoiles" />
+      </div>
+      <div className="images-carousel" ref={carouselRef}>
+        {selectedProject.images.map((file, index) => {
+          const isVideo = file.endsWith(".mp4") || file.endsWith(".mov");
 
-    return(
-        <main>
-            <div className="detail">
-                <h1>{selectedProject.title}</h1>
-                <h4>{selectedProject.date}</h4>
-                <br />
-                <p>{selectedProject.description}</p>
-                <br />
-                <img src={StarsProject} alt="etoiles" />
-            </div>
-            <div className="images-carousel">
-                {selectedProject.images.map((file, index) => {
-                    const isVideo = file.endsWith(".mp4") || file.endsWith(".mov");
-
-                    return isVideo ? (
-                    <video
-                        key={index}
-                        src={`/projects/${selectedKey}/${file}`}
-                        controls
-                        className="carousel-item"
-                    />
-                    ) : (
-                    <img
-                        key={index}
-                        src={`/projects/${selectedKey}/${file}`}
-                        alt={`${selectedProject.title} ${index + 1}`}
-                        className="carousel-item"
-                    />
-                    );
-                })}
-            </div>
-        </main>
-    );
+          return isVideo ? (
+            <video
+              key={index}
+              src={`/projects/${selectedKey}/${file}`}
+              controls
+              className="carousel-item"
+            />
+          ) : (
+            <img
+              key={index}
+              src={`/projects/${selectedKey}/${file}`}
+              alt={`${selectedProject.title} ${index + 1}`}
+              className="carousel-item"
+            />
+          );
+        })}
+      </div>
+    </main>
+  );
 }
