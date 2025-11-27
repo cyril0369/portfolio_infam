055a3c004173a6479f766c43e500b0362eb2c5a9
cyril <cyril@haberer.fr>
2025-11-27 13:21:52 +0100

    add ./ in the project paths

--- src/components/projects_display.jsx
+++ src/components/projects_display.jsx
@@ -26,14 +26,14 @@ export default function ProjectsDisplay({ selectedProject, selectedKey }) {
           return isVideo ? (
             <video
               key={index}
-              src={`/projects/${selectedKey}/${file}`}
+              src={`./projects/${selectedKey}/${file}`}
               controls
               className="carousel-item"
             />
           ) : (
             <img
               key={index}
-              src={`/projects/${selectedKey}/${file}`}
+              src={`./projects/${selectedKey}/${file}`}
               alt={`${selectedProject.title} ${index + 1}`}
               className="carousel-item"
             />
